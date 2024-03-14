fd2ebb301defff348ab8491282a21bba2fb9a262
iowosyse <iowosyse@gmail.com>
2024-03-14 09:36:35 -0600

    v1.1

---
+++ src/Employees/BasePlusCommissionEmployee.java
@@ -0,0 +1,24 @@
+package Employees;
+
+public class BasePlusCommissionEmployee extends CommissionEmployee{
+    private double baseSalary;
+
+    public BasePlusCommissionEmployee(String firstName, String lastName, String socialSecurityNumber, double grossSales,
+                                      double commissionRate, double baseSalary) {
+        super(firstName, lastName, socialSecurityNumber, grossSales, commissionRate);
+        this.baseSalary = baseSalary;
+    }
+
+    @Override
+    public double calculateEarnings() {
+        return super.calculateEarnings() + baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary){
+        this.baseSalary = baseSalary;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,22 @@ import Employees.*;
 
 public class Main {
     public static void main(String[] args) {
-        CommissionEmployee cande = new CommissionEmployee("Cande", "Ortega", "6180",
-                15000, 0.2);
+//        CommissionEmployee cande = new CommissionEmployee("Cande", "Ortega", "6180",
+//                15000, 0.2);
+//
+//        System.out.println("El vendedor cande vendió $15 000 y cobra comisión de 20%");
+//        System.out.println("Se le debe pagar a cande $" + cande.calculateEarnings());
+//
+//        cande.setCommissionRate(0.10);
+//        cande.setGrossSales(100_000);
+//
+//        System.out.println("El vendedor cande vendió $100 000 y cobra comisión de 10%");
+//        System.out.println("Se le debe pagar a cande $" + cande.calculateEarnings());
 
-        System.out.println("El vendedor cande vendió $15 000 y cobra comisión de 20%");
-        System.out.println("Se le debe pagar a cande $" + cande.calculateEarnings());
+        BasePlusCommissionEmployee celes = new BasePlusCommissionEmployee("Celes", "Arredondo", "6182",
+                4600, 0.15, 900);
 
-        cande.setCommissionRate(0.10);
-        cande.setGrossSales(100_000);
-
-        System.out.println("El vendedor cande vendió $100 000 y cobra comisión de 10%");
-        System.out.println("Se le debe pagar a cande $" + cande.calculateEarnings());
+        System.out.println("El empleado celes vendió $4600 y cobra comisión del 15% y tiene un salario de $900 por día");
+        System.out.println("Las ganancias de celes el día de hoy son: $" + celes.calculateEarnings());
     }
 }
